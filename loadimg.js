5c69f1e3f7a6f13309cad14a1921a9fe537b21c2
Ashish Singh <ashishsingh@renyi.cs.umass.edu>
2020-09-24 15:32:17 -0400

    changing index.html(5)

--- loadimg.js
+++ loadimg.js
@@ -1,12 +1,13 @@
-var folder = "img/";
-
+var dir = "img/";
+var fileextension = ".jpg";
 $.ajax({
-    url : folder,
+    //This will retrieve the contents of the folder if the folder is configured as 'browsable'
+    url: dir,
     success: function (data) {
-        $(data).find("a").attr("href", function (i, val) {
-            if( val.match(/\.(jpe?g|png|gif)$/) ) { 
-                $("body").append( "<img src='"+ folder + val +"'>" );
-            } 
-        });
+    //List all .png file names in the page
+    $(data).find("a:contains(" + fileextension + ")").each(function () {
+    var filename = this.href.replace(window.location, "");
+    $("body").append("<img src='" + dir + filename + "'>");
+    });
     }
-});
+    });
