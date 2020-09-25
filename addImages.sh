5206f622a8a8abe70b865a1b4745311d0c5c980a
Ashish Singh <ashishsingh@renyi.cs.umass.edu>
2020-09-24 21:19:09 -0400

    minor change

--- addImages.sh
+++ addImages.sh
@@ -1,3 +1,4 @@
 sed -i '/<div class="gg-box">/,/<\/div>/{//!d}' index.html
 for i in eb1/*.jpg; do echo "<img src='$i' />" >> img1.html; done;
 sed -i '/<div class="gg-box">/r img1.html' index.html
+rm img1.html
